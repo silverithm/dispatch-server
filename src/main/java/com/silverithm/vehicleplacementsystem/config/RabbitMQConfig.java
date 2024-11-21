6bd81c9f461e1bef32d45f0fede0e5e8ae27dbf3
jun <ggprgrkjh@naver.com>
2024-11-21 23:53:23 +0900

    feat(rabbitmq): RabbitMQ connectionFactory 추가

--- src/main/java/com/silverithm/vehicleplacementsystem/config/RabbitMQConfig.java
+++ src/main/java/com/silverithm/vehicleplacementsystem/config/RabbitMQConfig.java
@@ -6,14 +6,30 @@ import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.port}")
+    private int port;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+
     @Bean
     public Queue dispatchQueue() {
         return QueueBuilder.durable("dispatch.queue")
@@ -21,6 +37,7 @@ public class RabbitMQConfig {
                 .withArgument("x-dead-letter-routing-key", "dispatch.dead")
                 .build();
     }
+
 
     @Bean
     public Queue responseQueue() {
@@ -67,5 +84,18 @@ public class RabbitMQConfig {
         RabbitTemplate template = new RabbitTemplate(connectionFactory);
         template.setMessageConverter(jsonMessageConverter());
         return template;
+    }
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        com.rabbitmq.client.ConnectionFactory rabbitFactory = new com.rabbitmq.client.ConnectionFactory();
+        rabbitFactory.setMaxInboundMessageBodySize(1024 * 1024);
+        rabbitFactory.setHost(host);
+        rabbitFactory.setPort(port);
+        rabbitFactory.setUsername(username);
+        rabbitFactory.setPassword(password);
+
+        CachingConnectionFactory factory = new CachingConnectionFactory(rabbitFactory);
+        return factory;
     }
 }
